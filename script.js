820a086b830741f5735e453bcbb0c48539e94048
Freezing Moon <m.becraft1234@gmail.com>
2022-04-18 15:56:51 -0400

    Add function to play first to 5
    Also added a function to display the winner of the game.

--- script.js
+++ script.js
@@ -5,33 +5,74 @@ function computerPlay() {
   let choice = choices[random];
   return choice;
 }
+
+let playerWins = 0;
+let computerWins = 0;
+
 // plays a round and decides who wins
+// adds to the winners count
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
     return 'It\'s a tie! You both chose ' + playerChoice;
 
   } else if(playerChoice === 'rock' && computerChoice === 'paper') {
+      computerWins++;
       return 'You Lose! Paper beats Rock';
 
   } else if(playerChoice === 'rock' && computerChoice === 'scissors') {
+      playerWins++;
       return 'You win! Rock beats Scissors';
 
   } else if(playerChoice === 'paper' && computerChoice === 'rock') {
+      playerWins++;
       return 'You win! Paper beats Rock';
 
   } else if(playerChoice === 'paper' && computerChoice === 'scissors') {
+      computerWins++;
       return 'You lose! Scissors beats Paper';
 
   } else if(playerChoice === 'scissors' && computerChoice === 'rock') {
+      computerWins++;
       return 'You lose! Rock beats Scissors';
 
   } else if(playerChoice === 'scissors' && computerChoice === 'paper') {
+      playerWins++;
       return 'You win! Scissors beats Paper';
   }
 }
 
-const playerChoice = 'rock';
-const computerChoice = computerPlay();
-console.log(playRound(playerChoice, computerChoice));
-console.log(playRound('paper', computerChoice));
-console.log(playRound('scissors', computerChoice));
+// decide who wins the game
+// if player wins 5, player wins
+// if computer wins 5, computer wins
+// note: game stops after 5 wins by either one
+function winner(playerWins, computerWins) {
+  if(playerWins === 5) {
+    return 'You won the game!';
+  
+  } else if(computerWins === 5){
+    return 'You lost the game!';
+  }
+}
+
+// play a game, first to 5 wins
+// count number of rounds
+// display output of each round
+// if player is first to 5, player wins
+// else player loses
+// display who wins the game
+function game() {
+  playerWins;
+  computerWins;
+
+  for(let i = 0; (playerWins < 5 && computerWins < 5); i++) {
+    let round = i + 1;
+    const playerChoice = 'rock';
+    const computerChoice = computerPlay();
+    console.log('round: ' + round);
+    console.log(playRound(playerChoice, computerChoice));
+    console.log('player wins: ' + playerWins, 'computer wins: ' + computerWins);
+  }
+  return winner(playerWins, computerWins);
+}
+
+console.log(game());
